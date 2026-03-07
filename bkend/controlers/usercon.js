2f07bf853b899744dcb65c2640885181e97c0f0e
sekhar <irsrfsd@gmail.com>
2026-03-07 16:07:21 +0530

    ok

--- bkend/controlers/usercon.js
+++ bkend/controlers/usercon.js
@@ -1,6 +1,6 @@
 const um = require("../models/usermode")
 let bcrypt=require("bcrypt")
-
+let jwt=require("jsonwebtoken")
 let addstd=async(req,res)=>{
     try{
         let arr=await um.find({$or:[{"email":req.body.email},{"phno":req.body.phno}]})
@@ -25,4 +25,70 @@ let addstd=async(req,res)=>{
     }
 }
 
-module.exports=addstd
+let login=async(req,res)=>{
+    try{
+        if(/^[0-9]{6}$/.test(req.body.uid))
+        {
+            let obj=await um.findById(req.body.uid)
+            if(obj)
+            {
+                let f=await bcrypt.compare(req.body.pwd,obj.pwd)
+
+                if(f)
+                {
+                    res.json({"token":jwt.sign({"uid":obj.email},"1234"),"name":obj.name,"role":obj.role})
+                }
+                else{
+                    res.json({"msg":"check pwd"})
+                }
+
+            }
+            else{
+                res.json({"msg":"check hno"})
+            }
+
+        }
+        else{
+            let arr=await um.find({$or:[{"email":req.body.uid},{"phno":req.body.uid}]})
+            if(arr.length>0)
+            {
+                      let f=await bcrypt.compare(req.body.pwd,arr[0].pwd)
+                if(f)
+                {
+                    res.json({"token":jwt.sign({"uid":arr[0].email},"1234"),"name":arr[0].name,"role":arr[0].role})
+                }
+                else{
+                    res.json({"msg":"check pwd"})
+                }
+            }
+            else{
+               res.json({"msg":"check phno or email"}) 
+            }
+
+        }
+
+    }
+    catch(err)
+    {
+        console.log(err)
+         res.json({"msg":"error in login"})
+    }
+}
+
+let updmarks=async(req,res)=>{
+    try{
+      let data= await um.findByIdAndUpdate({"_id":req.body._id},{$push:{"marks":{$each:[req.body.sub1,req.body.sub2,req.body.sub3,req.body.sub4]}}})
+      console.log(data)
+        res.json({"msg":"marks updated" })
+
+    }
+    catch(err)
+    {
+        console.log(err)
+        res.json({"msg":"error in updating marks"})
+
+    }
+}
+
+
+module.exports={addstd,login,updmarks}
--- bkend/routes/rt.js
+++ bkend/routes/rt.js
@@ -1,5 +1,7 @@
 let express=require("express")
-const addstd = require("../controlers/usercon")
+const {addstd,login, updmarks} = require("../controlers/usercon")
 let rt=new express.Router()
 rt.post("/reg",addstd)
+rt.post("/login",login)
+rt.put("/updmarks",updmarks)
 module.exports=rt
