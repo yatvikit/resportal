962795edcbea811d84320616c8a40998dd1a7ed1
sekhar <irsrfsd@gmail.com>
2026-03-10 18:48:59 +0530

    display and edit marks admin

--- fend/src/comp/Logout.jsx
+++ fend/src/comp/Logout.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect } from 'react'
 import Ct from './Ct'
 import { useNavigate } from 'react-router-dom'
+import Cookies from 'js-cookie'
 
 const Logout = () => {
   let obj=useContext(Ct)
   let navigate=useNavigate()
   useEffect(()=>{
     obj.updstate({"token":"","hno":"","name":"","role":""})
+Cookies.remove("lgdet")
     navigate("/")
 
 
